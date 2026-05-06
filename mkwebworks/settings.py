19f1527cc72459de237d65f7fd2bc9622cb820ce
Kristian Maryna <kristianmaryna13@gmail.com>
2026-05-06 21:58:40 +0200

    Configurare finala pentru Render si corectare bug-uri

--- mkwebworks/settings.py
+++ mkwebworks/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -101,10 +101,9 @@ WSGI_APPLICATION = 'mkwebworks.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
