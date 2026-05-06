12a4f1a873b8860795f2fe8bcfed3d1783fca16a
Kristian Maryna <kristianmaryna13@gmail.com>
2026-05-06 22:45:20 +0200

    Fix absolute path error and static files

--- mkwebworks/settings.py
+++ mkwebworks/settings.py
@@ -163,13 +163,21 @@ STATICFILES_DIRS = [
     BASE_DIR / 'main' / 'static',  # Подключаем твою кастомную папку со static
 ]
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Куда будет складываться вся статика при collectstatic
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
