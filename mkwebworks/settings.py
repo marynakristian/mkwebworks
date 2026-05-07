3b2690d18863e2a4259e6e162431a23beb18e1d9
Kristian Maryna <kristianmaryna13@gmail.com>
2026-05-07 16:47:49 +0200

    Fix email timeout and port

--- mkwebworks/settings.py
+++ mkwebworks/settings.py
@@ -107,11 +107,9 @@ WHITENOISE_USE_FINDERS = True
 # 8. EMAIL SETTINGS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465               # Portul 465 este mai stabil pe Render decât 587
-EMAIL_USE_TLS = False          # SSL folosește TLS=False
-EMAIL_USE_SSL = True           # Activat
-EMAIL_HOST_USER = 'kristianmaryna13@gmail.com'
-# Ia parola din variabila de mediu Render (CEA NOUĂ pe care o generezi acum)
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('ibln kmpa xmcu kiqu') 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
