58f26aff3b383f40a8b3405c953f3237d9a2ea89
Kristian Maryna <kristianmaryna13@gmail.com>
2026-05-07 14:09:55 +0200

    Fixed email notification and routing

--- main/urls.py
+++ main/urls.py
@@ -1,16 +1,18 @@
 from django.urls import include, path
 from . import views
-from .views import contact_view
-from django.conf.urls.i18n import set_language
 
+# Nu mai avem nevoie de 'from .views import contact_view' separat dacă importăm deja 'views'
 
 urlpatterns = [
+    # Ruta principală care încarcă pagina cu formularul
     path('', views.home_view, name='index'),
+    
+    # Ruta pentru pagina de contact (asigură-te că funcția se numește contact_view în views.py)
     path('contact/', views.contact_view, name='contact'),
+    
+    # Ruta pentru procesarea limbii (dacă încă o folosești)
     path('i18n/', include('django.conf.urls.i18n')),
-    path('contact/submit/', views.contact_view, name='submit_contact_form'),  # если хотите так
-
-
+    
+    # Dacă formularul tău din HTML are action="{% url 'submit_contact_form' %}"
+    path('contact/submit/', views.contact_view, name='submit_contact_form'),
 ]
-  
-  
