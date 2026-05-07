72329d7eb80c4a7928036c307dd91e420b67f0c1
Kristian Maryna <kristianmaryna13@gmail.com>
2026-05-07 16:50:15 +0200

    Fix email timeout and port

--- mkwebworks/settings.py
+++ mkwebworks/settings.py
@@ -109,7 +109,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_USER = 'kristianmaryna13@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('ibln kmpa xmcu kiqu') 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
