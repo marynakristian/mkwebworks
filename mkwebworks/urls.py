096b42f879d7169332ba4606b71ec2db16bad3b7
Kristian Maryna <kristianmaryna13@gmail.com>
2026-05-08 10:41:48 +0200

    Manual override fix for 500 error

--- mkwebworks/urls.py
+++ mkwebworks/urls.py
@@ -3,12 +3,10 @@ from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    # Rute care NU se traduc (Admin și schimbătorul de limbă)
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
-# Rutele aplicației tale, care vor avea prefix de limbă (/en/, /ro/ etc.)
 urlpatterns += i18n_patterns(
     path('', include('main.urls')),
 )
